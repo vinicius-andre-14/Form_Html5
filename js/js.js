77be10ec8943a77d9485473a8f9af08c631d3302
vinicius-andre-14 <vinicius.eumesmo.14@gmail.com>
2021-05-05 22:44:42 -0300

    Máscaras e VIACEP

--- js/js.js
+++ js/js.js
@@ -1,8 +1,4 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict'
   // Pega todos os formulários que desejamos aplicar o estilo do Bootstrap
@@ -19,14 +15,14 @@
       }, false)
     })
 })()
-
-
-//https://pt.stackoverflow.com/questions/53275/m%c3%a1scaras-para-telefone-e-cpf-em-textfield  
+// Copiar essas funções
+//https://pt.stackoverflow.com/questions/53275/m%c3%a1scaras-para-telefone-e-cpf-em-textfield
 function mascara(objeto,funcao){
     v_obj=objeto
     v_fun=funcao
     setTimeout("execmascara()",1);
 }
+
 function execmascara(){
     v_obj.value=v_fun(v_obj.value);
 }
@@ -34,17 +30,6 @@ function execmascara(){
 function id( objeto ){
 	return document.getElementById( objeto );
 }
-
-
-function mcpf(v){  //MASCARA PARA CPF
-    v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-                                             //de novo (para o segundo bloco de números)
-    v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2") //Coloca um hífen entre o terceiro e o quarto dígitos
-    return v;
-}
-
 
 function mtel(v){ //MASCARA PARA TELEFONE
 
@@ -55,6 +40,16 @@ function mtel(v){ //MASCARA PARA TELEFONE
 }
 
 
+function mcpf(v){  //MASCARA PARA CPF
+
+    v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito
+    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
+    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
+                                             //de novo (para o segundo bloco de números)
+    v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2") //Coloca um hífen entre o terceiro e o quarto dígitos
+    return v;
+}
+
 function mcnpj(v){  //MASCARA PARA CNPJ
      v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito	
 	   v=v.replace(/(\d{2})(\d)/,"$1.$2")
@@ -63,7 +58,6 @@ function mcnpj(v){  //MASCARA PARA CNPJ
      v=v.replace(/(\d)(\d{2})$/,"$1-$2");    //Coloca o . antes dos últimos 3 dígitos, e antes do verificador 
      return v;
 }
-
 
 function mie(v){  //MASCARA PARA CNPJ
     v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito      	 
@@ -137,6 +131,82 @@ function memail(v){
 		v=v.replace( /\s/g, '' );
 		return v;
 }
+
+
+
+
+
+
+
+//via cep
+ 
+    function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('rua').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('uf').value=("");
+            document.getElementById('ibge').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+            //document.getElementById('ibge').value=(conteudo.ibge);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('rua').value="Carregando...";
+                document.getElementById('bairro').value="Carregando...";
+                document.getElementById('cidade').value="Carregando...";
+                document.getElementById('uf').value="Carregando...";
+              //  document.getElementById('ibge').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
     window.onload = function() {
     var btnModoNoturno = document.getElementById('modo');
 
@@ -156,6 +226,12 @@ function memail(v){
     id('txtCPF').onkeyup = function(){
           mascara(this,mcpf);
       }
+    id('cep').onkeyup = function(){ 
+		mascara( this, mcep)
+	}
+    id('txtCEL').onkeyup = function(){ 
+		mascara( this, mtel)
+	}  
 }
     
 
